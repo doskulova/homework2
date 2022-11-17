e20898dc65f4c56d15cee0320520b9538611df78
doskulova <110982600+doskulova@users.noreply.github.com>
2022-11-17 15:51:16 +0600

    first commit

---
+++ src/pages/Pages.js
@@ -0,0 +1,14 @@
+
+
+
+function AboutUs(props){
+    return(
+        <>
+        <p>title:{props.textInfo.title}</p>
+        <p>subtitle:{props.textInfo.subtitle}</p>
+        </>
+
+    )
+}
+
+export default AboutUs;
